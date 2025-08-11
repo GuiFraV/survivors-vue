ce9b35748f8634fd3238f4ffd5f616aa725433af
GuiFraV <guillaume.franguiadakis@gmail.com>
2025-08-11 18:40:39 +0200

    Starting project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import GameHUD from './components/GameHUD.vue';
+
+const state = reactive({
+  hp: 100,
+  xp: 0,
+});
+
+function gainXp(v: number) {
+  state.xp += v;
+}
+
+function takeDamage(v: number) {
+  state.hp = Math.max(0, state.hp);
+}
+
+</script>
+
+<template>
+  <GameHUD :hp="state.hp" :xp="state.xp" @gain-xp="gainXp"/>
+  <button @click="takeDamage(10)">Take Damage</button>
+</template>
---
+++ src/components/GameHUD.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  hp: number;
+  xp: number;
+}>();
+const emit = defineEmits<{
+  (e: 'gain-xp', value: number): void;
+}>();
+</script>
+
+<template>
+    <div class="hud">
+        <div>HP: {{ props.hp }}</div>
+        <div>XP: {{ props.xp }}</div>
+        <button @click="emit('gain-xp', 5)">+5 XP</button>
+    </div>
+</template>
